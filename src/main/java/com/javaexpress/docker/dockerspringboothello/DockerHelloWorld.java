3df7ae38035a647c98e98557ecca16e4c1323283
unknown <sunny.metkar@gmail.com>
2023-03-13 23:36:31 +0530

    updated mappings

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/docker")
 public class DockerHelloWorld {
 
-	@GetMapping("/name")
+	@GetMapping("/docker")
 	public String getName() {
 		
 		return "Welcome to deployment in docker";
